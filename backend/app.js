6e776261915b29a6bfa8e9033b89fcf9578929d8
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2024-09-06 16:28:25 +0530

    Update app.js
    added employeeRoutes and adminRoutes

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 const cors=require('cors');
 const authRoutes = require('../backend/routes/authRoutes');
-
+const employeeRoutes=require('../backend/routes/employeeRoutes');
+const adminRoutes = require('../backend/routes/adminRoutes');
+const authenticateToken = require('./middleware/autheticate');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,18 +14,23 @@ const db=process.env.MONGO_URI;
 // MongoDB database here
 mongoose.connect(db, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS:30000
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
 
 //middlewares
 app.use(cors())
+// app.use(authenticateToken());
 // Parse incoming JSON data
 // app.use(parseJSON());
 
 app.use(express.json());
 app.use('/api/auth', authRoutes); 
+app.use("/api/employee", employeeRoutes);
+app.use("/api/admin",adminRoutes);
+
 
 //listeners
 app.listen(PORT, () => {
